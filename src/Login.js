2fa1e53666a58b4fe67b1fcda0bd61d170774c3a
Bibubobee <gaboenix.2@gmail.com>
2023-08-30 20:58:34 -0400

    CENTERED IT

--- src/Login.js
+++ src/Login.js
@@ -2,41 +2,37 @@ import "./Login.css"
 
 function Login(){
     return(
-        <div class="container">
-            <div class="row">
-                <div class="mx-auto my-5 col-10 col-md-8 col-lg-6">
-                    <form class="form-example" action="" method="post">
-                    <h1>Inicie Sesión</h1>
-                    <p class="description">
-                        Coloque su correo y contraseña de la biblioteca para hacer el pedido.
-                    </p>
-                    <div class="form-group">
-                        <label for="username">Correo:</label>
-                        <input
-                        type="text"
-                        class="form-control username"
-                        id="username"
-                        placeholder="Correo Electronico"
-                        name="username"
-                        />
-                    </div>
-                    <div class="form-group">
-                        <label for="password">Contraseña:</label>
-                        <input
-                        type="password"
-                        class="form-control password"
-                        id="password"
-                        placeholder="Contraseña"
-                        name="password"
-                        />
-                    </div>
-        
-                    <button type="submit" class="btn btn-primary btn-customized mt-4">
-                        Pedir Libro
-                    </button>
-                    </form>
-                </div>
+        <div className="myLogin color-overlay d-flex justify-content-center align-items-center">
+            <form className="rounded p-4 p-sm-3" action="" method="post">
+            <h1>Inicie Sesión</h1>
+            <p class="description">
+                Coloque su correo y contraseña de la biblioteca para hacer el pedido.
+            </p>
+            <div className="form-group mb-2">
+                <label for="username">Correo:</label>
+                <input
+                type="text"
+                class="form-control username"
+                id="username"
+                placeholder="Correo Electronico"
+                name="username"
+                />
             </div>
+            <div class="form-group">
+                <label for="password">Contraseña:</label>
+                <input
+                type="password"
+                class="form-control password"
+                id="password"
+                placeholder="Contraseña"
+                name="password"
+                />
+            </div>
+
+            <button type="submit" class="btn btn-primary btn-customized mt-4">
+                Pedir Libro
+            </button>
+            </form>
         </div>
     )
 }
