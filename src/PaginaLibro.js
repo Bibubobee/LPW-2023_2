5a2ca97bfc15f280c61712b74043ecd28f8388b5
Daniel-Parraguez <daniel.parraguezs@usm.cl>
2023-09-11 15:49:20 -0300

    Actualizacion
    Sin navbar y un copypaste del Searchbar.css en el PaginaLibro.css

--- src/PaginaLibro.js
+++ src/PaginaLibro.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import './App.css';
+import './PaginaLibro.css';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
@@ -13,34 +13,9 @@ import "bootstrap/dist/css/bootstrap.min.css"
 
 function PaginaLibro() {
   return (
-    <div className="App">
-      <header>
-        <Navbar bg="dark" variant="dark" expand="lg">
-          <Container>
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">Link</Nav.Link>
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">
-                    Another action
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action/3.4">
-                    Separated link
-                  </NavDropdown.Item>
-                </NavDropdown>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-      </header>
+    <div class="PaginaLibro">
       <main>
-        <Container>
+        <div class="container">
           <Row className='px-4 my-5'>
             <Col sm={7}>
               <Image src="https://imagessl8.casadellibro.com/a/l/t7/28/9788408175728.jpg" 
@@ -69,7 +44,7 @@ function PaginaLibro() {
               </div>
             </Col>
           </Row>
-        </Container>
+        </div>
       </main>
     </div>
   );
