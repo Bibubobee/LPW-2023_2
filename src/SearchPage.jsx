5d9e42dc539b68d148492f71b84085f38f710f51
Bibubobee <gaboenix.2@gmail.com>
2023-09-11 00:50:34 -0300

    Styling mostly finished

--- src/SearchPage.jsx
+++ src/SearchPage.jsx
@@ -2,17 +2,21 @@ import "./SearchPage.css"
 
 function SearchPage(){
     return (
-        <body>
-            <div> 
+        <body class="body">
+            <div class="container"> 
                 <div class="row g-3 pt-5">
-                    <div class="col-10 mx-auto">
+                    <div class="col-12 mx-auto">
                         <div style={{height:80}}>
                             <input type="text" class="form-control form-control-lg h-100" placeholder="Nombre del Libro"/>
                         </div>
                     </div>
+
+                    <div class="col-12 mx-auto pt-1">
+                        <button type="submit" class="btn btn-success">Buscar</button>
+                    </div>
                 </div>
 
-                <div class="row g-3 pt-3 pb-3">
+                {/* <div class="row g-3 pt-3 pb-3">
                     <div class="col-10 mx-auto">
                         <label>Seleccionar Género</label>
                         <select class="form-select">
@@ -21,14 +25,8 @@ function SearchPage(){
                             <option value="3">Romance</option>
                         </select>
                     </div>
-                </div>
-            </div>
-
-                <div class="col-10 mx-auto">
-                    <button type="submit" class="btn btn-success">Buscar</button>
-                </div>
-            <div class="container">
-                <div class="row mt-5">
+                </div> */}
+                <div class="row mt-5 mx-auto">
                     {BookProduct()}
                     {BookProduct()}
                     {BookProduct()}
@@ -45,18 +43,19 @@ function SearchPage(){
 function BookProduct(){
     return(
         <div class="col-lg-3 col-sm-12 col-md-6 mt-3">
-            <a class="card" href="#!" style={{"textDecoration": "None"}}>                
-                <div class="card-body bg-light">
-                    <div class="text-center">
-                        <h4 class="card-title font-weight-semibold">El Manifiesto Comunista</h4>
-                        <div class="card-img-actions">
-                            <img src={require('./temp.jpg')} class="card-img img-fluid" width="100" height="150" alt=""/>
-                        </div>
-                    </div>
-                    <div class="text-left">
+            <a class="card" href="#!" style={{"textDecoration": "None"}}>   
+                <div class="text-center pb-3 pt-3">
+                    <img src={require('./temp.jpg')} class="card-image-top img-shadow" width="60%" height="auto" alt=""/>
+                </div>             
+                <div class="card-body">
+                    <h5 class="text-center card-title fw-bold">El Manifiesto Comunista</h5>
+                    <h6 class="text-center">Karl Marx & Friedrich Engels</h6>
+                    
+                    
+                    {/* <div class="text-left">
                         <h6 class="text-muted mt-2">Autor: Karl Marx & Friedrich Engels</h6>
                         <h6 class="text-muted">Stock: 10</h6>
-                    </div>
+                    </div> */}
                 </div>
             </a>
         </div>
